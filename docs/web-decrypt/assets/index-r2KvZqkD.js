f260f0b105d3af8bb4b2057e99335f83e1a701fc
mcampetta <martin.campetta@gmail.com>
2026-03-13 14:49:43 -0500

    Version Web Decrypt sample asset again

--- docs/web-decrypt/assets/index-r2KvZqkD.js
+++ docs/web-decrypt/assets/index-r2KvZqkD.js
@@ -30,7 +30,7 @@
           <div id="sampleBox" class="sample-box">
             <p class="sample-title">Need a test package?</p>
             <p class="sample-copy">
-              <a class="sample-link" href="./WebDecryptSample_v2_SecureDelivery.zip">Download sample package</a>
+              <a class="sample-link" href="./WebDecryptSample_v3_SecureDelivery.zip">Download sample package</a>
               <span class="sample-password">Password: <code>obsidianq-demo</code></span>
             </p>
           </div>
